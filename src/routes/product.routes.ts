36bbedd54be8d1c55f981836b2542988bb485ac7
rlopezse <rza.lopez.s@gmail.com>
2026-04-12 22:49:34 -0400

    added: redis & middleware

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { productController } from '../controllers/product.controller'
+import { validate } from '../middlewares/validate.middleware';
+import { createProductSchema, updateProductSchema } from '../schemas/product.schema';
 
 const router = Router()
 
 router.get('/', productController.getAll)
 router.get('/:id', productController.getById)
-router.post('/', productController.create)
+router.post('/', validate(createProductSchema), productController.create)
 router.put('/:id', productController.update)
 router.delete('/:id', productController.delete)
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { userController } from '../controllers/user.controller'
+import { validate } from '../middlewares/validate.middleware';
+import { createUserSchema, updateUserSchema } from '../schemas/user.schema';
 
 const router = Router()
 
 router.get('/', userController.getAll)
 router.get('/:id', userController.getById)
-router.post('/', userController.create)
-router.put('/:id', userController.update)
+router.post('/', validate(createUserSchema), userController.create)
+router.put('/:id', validate(updateUserSchema), userController.update)
 router.delete('/:id', userController.delete)
 
 export default router
